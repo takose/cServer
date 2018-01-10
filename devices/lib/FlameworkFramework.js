dd9bab721631b788b051ab62622296ffd714589b
tako <rina.m.takase@gmail.com>
2018-01-10 20:21:49 +0900

    Implament fetch data for device

--- devices/lib/FlameworkFramework.js
+++ devices/lib/FlameworkFramework.js
@@ -6,6 +6,12 @@ class FlameworkFramework extends Device {
     this.state = {
       power: 0,
     };
+    this.init();
+  }
+  init() {
+    this.socket.on('devices/state:fetch', () => {
+      this.socket.emit('devices/state:fetch/return', this.state);
+    });
   }
 }
 
