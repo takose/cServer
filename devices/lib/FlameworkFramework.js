5f9861bb4116b776df88903343c5cfa3238f9a4a
tako <rina.m.takase@gmail.com>
2018-03-04 11:26:06 +0900

    USB Serial

--- devices/lib/FlameworkFramework.js
+++ devices/lib/FlameworkFramework.js
@@ -1,3 +1,5 @@
+const SerialPort = require('serialport');
+
 const Device = require('./Device');
 
 class FlameworkFramework extends Device {
@@ -10,6 +12,18 @@ class FlameworkFramework extends Device {
     this.init();
   }
   init() {
+    this.port = new SerialPort('/dev/tty.lpss-serial2', {
+      baudRate: 57600,
+    });
+
+    this.port.on('error', (err) => {
+      console.log('Error: ', err.message);
+    });
+
+    this.port.on('data', (data) => {
+      console.log('Data:', data);
+    });
+
     this.socket.on('devices/state:fetch', () => {
       this.socket.emit('devices/state:fetch/return', this.state);
     });
@@ -40,10 +54,33 @@ class FlameworkFramework extends Device {
 
   setPower(val) {
     this.state.power = val;
+    let power;
+    // magic number..
+    if (val === 0) {
+      power = 10;
+    } else if (val === 1) {
+      power = 100;
+    } else if (val === 2) {
+      power = 120;
+    } else if (val === 3) {
+      power = 160;
+    } else if (val === 4) {
+      power = 190;
+    } else if (val === 5) {
+      power = 230;
+    } else if (val === 6) {
+      power = 256;
+    }
+    this.port.write(`${power}\n`, (err) => {
+      if (err) {
+        console.log('Error on write: ', err.message);
+      }
+    });
   }
 
   measureTime(val) {
     this.state.time = val;
+
     const count = setInterval(() => {
       this.state.time = this.state.time - 1;
       console.log(`time: ${this.state.time}`);
